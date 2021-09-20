b8e53b7019ec85637260f0299f912df07f3fb638
ermias kidane <ermiaskidanegebre@gmail.com>
2021-09-20 21:03:10 +0100

    first commit

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,14 +1,16 @@
 <script>
     import Hero from "../components/Hero.svelte";
-    import { Link } from "svelte-routing";
+    // import { Link } from "svelte-routing";
+    import { link } from "svelte-routing"
 
     //featured products
-    function getProps() {
-        return { class: "btn btn-primary btn-hero"}
-    }
+    // function getProps() {
+    //     return { class: "btn btn-primary btn-hero"}
+    // }
 
 </script>
 
 <Hero>
-    <Link to="/products" {getProps}>Shop now</Link>
+    <a href="/products" class="btn btn-primary btn-hero" use:link>Shop now</a>
+    <!-- <Link to="/products" {getProps}>Shop now</Link> -->
 </Hero>
