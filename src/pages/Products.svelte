8ac0bad883382dcf7b7eb46a0cb38aeb8ff38215
ermias kidane <ermiaskidanegebre@gmail.com>
2021-09-22 04:37:50 +0100

    added the subscribe and unsubscribe method

--- src/pages/Products.svelte
+++ src/pages/Products.svelte
@@ -1,1 +1,5 @@
-<h1>Welcome to product page</h1>
+<script>
+    import Products from "../components/Products/Products.svelte"
+</script>
+
+<Products />
---
+++ src/stores/defaultProducts.js
@@ -0,0 +1,31 @@
+import { writable } from "svelte/store";
+import localProducts from "../localProducts"
+
+const store = writable(flattenProducts([...localProducts]));
+
+// subscribe
+// set
+// update
+
+function flattenProducts(data) {
+    return data.map(item => {
+        let image = item.image.url;
+        return{...item, image};
+    })
+}
+
+export default store;
+
+// import { writable } from "svelte/store";
+// import localProducts from "../localProducts"
+
+// const store = writable([...localProducts]);
+
+// // subscribe
+// // set
+// // update
+
+// export default store;
+
+
+
