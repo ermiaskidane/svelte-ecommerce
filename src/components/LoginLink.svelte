89601ea705f77e95e8b1e826004d6575b370b404
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2021-10-04 22:14:41 +0100

    Create LoginLink.svelte

---
+++ src/components/LoginLink.svelte
@@ -0,0 +1,27 @@
+<script>
+    import user from "../stores/user";
+    import { link } from "svelte-routing";
+    // logout function
+    import globalStore from "../stores/globalStore"
+</script>
+
+{#if $user.jwt}
+    <a 
+    href="/" 
+    use:link
+    class="logout-btn" 
+    on:click={() => {
+        globalStore.toggleItem("sidebar", false)
+    }}>
+        Logout
+    </a>
+{:else}
+    <a 
+    href="/login" 
+    use:link
+    on:click={() => {
+        globalStore.toggleItem("sidebar", false)
+    }}>
+        Login
+    </a>
+{/if}
